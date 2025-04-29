ac1eb8477d457eaf36e146340a658270b9dd7e1b
Dhananjay8119 <yesitlabs.dhananjay@gmail.com>
2025-04-30 00:33:14 +0530

    DhananjaySingh 29_04_2024 updated
     29_04_2024 updated

--- app/src/main/java/com/mykaimeal/planner/adapter/AdapterOrderHistoryDetailsItem.kt
+++ app/src/main/java/com/mykaimeal/planner/adapter/AdapterOrderHistoryDetailsItem.kt
@@ -5,9 +5,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.mykaimeal.planner.databinding.AdapterOrderHistoryDetailsItemBinding
-import com.mykaimeal.planner.fragment.mainfragment.profilesetting.orderhistoryscreen.model.OrderHistoryModelData
+import com.mykaimeal.planner.fragment.mainfragment.profilesetting.orderhistoryscreen.model.Item
 
-class AdapterOrderHistoryDetailsItem(private var datalist: MutableList<OrderHistoryModelData>,
+class AdapterOrderHistoryDetailsItem(private var datalist: MutableList<Item>?,
                                      private var requireActivity: FragmentActivity
 ): RecyclerView.Adapter<AdapterOrderHistoryDetailsItem.ViewHolder>() {
 
@@ -20,12 +20,31 @@ class AdapterOrderHistoryDetailsItem(private var datalist: MutableList<OrderHist
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        val data= datalist[position]
+        val data = datalist?.get(position) ?: return
+
+        with(holder.binding) {
+            data.quantity?.let { tvNumber.text = it.toString() }
+            data.name?.let { tvIngredientName.text = it }
+
+            data.base_price?.let { price ->
+                val formattedPrice = price.div(100.0).let {
+                    if (it % 1.0 == 0.0) it.toInt().toString() else String.format("%.2f", it)
+                }
+                tvOrderPrice.text = formattedPrice
+            }
+        }
+
 
     }
 
     override fun getItemCount(): Int {
-        return datalist.size
+        return datalist!!.size
+    }
+
+    fun update(items: MutableList<Item>) {
+        datalist=items
+        notifyDataSetChanged()
+
     }
 
     class ViewHolder(var binding: AdapterOrderHistoryDetailsItemBinding) : RecyclerView.ViewHolder(binding.root){
